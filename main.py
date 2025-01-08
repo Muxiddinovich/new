c71c4f37351428268670f6cf33f4074153159955
Sherbek <sherbekkubayev@gmail.com>
2025-01-08 14:15:19 +0500

    shox yurishi qoshildi

--- main.py
+++ main.py
@@ -18,3 +18,13 @@ print(knight_check(4,4,5,6))
 
 def queen_check(x1,y1,x2,y2):
     return abs(x1-x2)==abs(y1-y2)or x1==x2 or y1==y2
+
+
+
+def king_check(x1,y1,x2,y2):
+    ox = abs(x1-x2)
+    oy=abs(y1-y2)
+
+    return (ox==1 and oy==1) or (ox==0 and oy==1) or (ox==1 and oy==0)
+
+print(king_check(2,1,2,2))

3b94b1305a08ddd7b9f76e404bdd855f7c56c288
Sherbek <sherbekkubayev@gmail.com>
2025-01-08 13:41:31 +0500

    ot yurishi qo'shildi

--- main.py
+++ main.py
@@ -5,6 +5,17 @@ def rook_check(x1,y1,x2,y2):
     return x1==x2 or y1==y2
 
 
-print(rook_check(1,1,8,2))
+def bishop_check(x1,y1,x2,y2):
+    return abs(x1-x2)==abs(y1-y2)
+
+
+
+def knight_check(x1,y1,x2,y2):
+    ox=abs(x1-x2)
+    oy=abs(y1-y2)
+
+    return (ox==2 and oy==1) or (ox==1 and oy==2)
+
+print(knight_check(4,4,5,6))
 
 

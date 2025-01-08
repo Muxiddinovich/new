487803e59c46bd1187bf10814eb8460f25e5a477
Sherbek <sherbekkubayev@gmail.com>
2025-01-08 14:04:20 +0500

    fil yurishi

--- main.py
+++ main.py
@@ -13,3 +13,8 @@ def bishop_check(x1,y1,x2,y2):
 def knight_check(x1,y1,x2,y2):
     return (x1-x2)**2 + (y1-y2)**2==5
 print(knight_check(4,4,5,6))
+
+
+
+def queen_check(x1,y1,x2,y2):
+    return abs(x1-x2)==abs(y1-y2)or x1==x2 or y1==y2
